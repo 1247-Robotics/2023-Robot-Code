51636fa368a6b4e7de0b857cf5f694c0d36714fc
OverlordPotato1 <kovrevik12@gmail.com>
2023-03-25 23:17:48 -0400

    made things better

--- src/main/java/frc/robot/Claw_Servo.java
+++ src/main/java/frc/robot/Claw_Servo.java
@@ -7,42 +7,63 @@ Just asked ChatGPT for a crash course in OOP in java so not my fault if this bre
  */
 
 public class Claw_Servo {
-    int PWMPort;
     private Servo servo;
     private int closedAngle;
     private int openAngle;
+    private int current;
+    private int currentImmune;
 
-    public Claw_Servo(int PWMPort) {
-        this.PWMPort = PWMPort;
-        this.servo = new Servo(PWMPort);
-
-    }
+    public Claw_Servo(int PWMPort) { this.servo = new Servo(PWMPort); }
 
     public void set(int Angle) {
+        this.current = Angle;
+        this.currentImmune = Angle;
         this.servo.setAngle(Angle);
     }
 
-    public void setClosed(int closed) {
-        this.closedAngle = closed;
-    }
+    public void setClosed(int closed) { this.closedAngle = closed; }
 
-    public void setOpen(int open) {
-        this.openAngle = open;
-    }
+    public void setOpen(int open) { this.openAngle = open; }
 
     public void close() {
+        this.current = this.closedAngle;
         this.servo.setAngle(this.closedAngle);
     }
     
     public void open() {
+        this.current = this.openAngle;
         this.servo.setAngle(this.openAngle);
     }
 
-    public int getClosed() {
-        return this.closedAngle;
+    public void openAsManual() {
+        this.current = this.openAngle;
+        this.currentImmune = this.openAngle;
+        this.servo.setAngle(this.openAngle);
     }
 
-    public int getOpen() {
-        return this.openAngle;
+    public void closeAsManual() {
+        this.current = this.closedAngle;
+        this.currentImmune = this.closedAngle;
+        this.servo.setAngle(this.closedAngle);
+    }
+
+    public int getClosed() { return this.closedAngle; }
+
+    public int getOpen() { return this.openAngle; }
+
+    public int current() { return this.current; }
+
+    public int getLastManual() { return this.currentImmune; }
+
+    public boolean isOpen() {
+        if (this.current == this.openAngle) { return true; } else { return false; }
+    }
+
+    public boolean isClosed() {
+        if (this.current == this.closedAngle) { return true; } else { return false; }
+    }
+
+    public void setOpen(boolean open) {
+        if (open) { this.open(); } else { this.close(); }
     }
 }
