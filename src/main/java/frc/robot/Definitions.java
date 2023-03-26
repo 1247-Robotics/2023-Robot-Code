233e2169b45fffbcc87dddd04dcc5b3d710a9346
OverlordPotato1 <kovrevik12@gmail.com>
2023-03-25 23:46:39 -0400

    cleared a lot of unused variables

--- src/main/java/frc/robot/Definitions.java
+++ src/main/java/frc/robot/Definitions.java
@@ -33,3 +33,32 @@ public class Definitions {
     public static int servo2Close = 40;
     public static int servo2Open = 90;
 }
+
+// public class SensorConstants {
+//     public static final int HMC5883L_ADDRESS        = 0x1E; // idfk what this is for
+//     public static final int HMC5883L_CONFIG_REG_A   = 0x00;
+//     public static final int HMC5883L_CONFIG_REG_B   = 0x01;
+//     public static final int HMC5883L_MODE_REG       = 0x02;
+//     public static final int HMC5883L_MEASURE_CONT   = 0x00;
+//     public static final int HMC5883L_MEASURE_SINGLE = 0x01;
+//     public static final int HMC5883L_MEASURE_IDLE   = 0x03;
+//     public static final int HMC5883L_DATA           = 0x03;
+//     public static final int HMC5883L_M_TO_READ      = 6;    // 2 bytes for each axis x, y, z
+//     public static final int BMP085_ADDRESS          = 0x77; //address of the Barometer
+    
+//     public static final int BMA180_ADDRESS   = 0x40; //address of the accelerometer
+//     public static final int BMA180_RESET     = 0x10;
+//     public static final int BMA180_PWR       = 0x0D;
+//     public static final int BMA180_BW        = 0x20;
+//     public static final int BMA180_RANGE     = 0x35;
+//     public static final int BMA180_DATA      = 0x02;
+//     public static final int BMA180_A_TO_READ = 6;    // 2 bytes for each axis x, y, z
+    
+//     public static final int ITG3205_ADDRESS    = 0x68; //address of the Gyroscope
+//     public static final int ITG3205_SMPLRT_DIV = 0x15;
+//     public static final int ITG3205_DLPF_FS    = 0x16;
+//     public static final int ITG3205_INT_CFG    = 0x17;
+//     public static final int ITG3205_PWR_MGM    = 0x3E;
+//     public static final int ITG3205_DATA       = 0x1B;
+//     public static final int ITG3205_G_TO_READ  = 8;    // 2 bytes for each axis x, y, z, temp    
+//   }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,43 +5,19 @@
 package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-// import edu.wpi.first.wpilibj.interfaces.Accelerometer;
-// import edu.wpi.first.wpilibj.motorcontrol.Talon;
-// import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import edu.wpi.first.wpilibj.BuiltInAccelerometer;
-// import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PS4Controller;
-
-// import com.revrobotics.CANEncoder;
-// import com.ctre.phoenix.motorcontrol.DemandType;
-// import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-// import edu.wpi.first.wpilibj.PS4Controller;
-// import the can talon srx
-// import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-// import java.sql.Driver;
 import com.revrobotics.CANSparkMax;
-// import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
-
-// import com.revrobotics.CANSparkMax.IdleMode;
-// import edu.wpi.first.cameraserver.CameraServer;
-// import edu.wpi.first.networktables.NetworkTableInstance;
-// import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.cscore.HttpCamera;
-// import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Timer;
 
 import frc.robot.Claw_Servo;
-
-
-// import edu.wpi.first.wpilibj.controller.PIDController;
-// import edu.wpi.first.wpilibj.controller.PIDSourceType;
 
 
 
@@ -53,42 +29,8 @@ import frc.robot.Claw_Servo;
  */
 public class Robot extends TimedRobot {
 
-  public class startCompetition {}
-
-  public class SensorConstants {
-    public static final int HMC5883L_ADDRESS = 0x1E; // idfk what this is for
-    public static final int HMC5883L_CONFIG_REG_A = 0x00;
-    public static final int HMC5883L_CONFIG_REG_B = 0x01;
-    public static final int HMC5883L_MODE_REG = 0x02;
-    public static final int HMC5883L_MEASURE_CONT = 0x00;
-    public static final int HMC5883L_MEASURE_SINGLE = 0x01;
-    public static final int HMC5883L_MEASURE_IDLE = 0x03;
-    public static final int HMC5883L_DATA = 0x03;
-    public static final int HMC5883L_M_TO_READ = 6; // 2 bytes for each axis x, y, z
-    public static final int BMP085_ADDRESS = 0x77; //address of the Barometer
-    
-    public static final int BMA180_ADDRESS = 0x40; //address of the accelerometer
-    public static final int BMA180_RESET = 0x10;
-    public static final int BMA180_PWR = 0x0D;
-    public static final int BMA180_BW = 0x20;
-    public static final int BMA180_RANGE = 0x35;
-    public static final int BMA180_DATA = 0x02;
-    public static final int BMA180_A_TO_READ = 6; // 2 bytes for each axis x, y, z
-    
-    public static final int ITG3205_ADDRESS = 0x68; //address of the Gyroscope
-    public static final int ITG3205_SMPLRT_DIV = 0x15;
-    public static final int ITG3205_DLPF_FS = 0x16;
-    public static final int ITG3205_INT_CFG = 0x17;
-    public static final int ITG3205_PWR_MGM = 0x3E;
-    public static final int ITG3205_DATA = 0x1B;
-    public static final int ITG3205_G_TO_READ = 8; // 2 bytes for each axis x, y, z, temp
-    
-    
-  }
-
-
   private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "Balance";
+  private static final String kCustomAuto  = "Balance";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
@@ -100,18 +42,16 @@ public class Robot extends TimedRobot {
 
 
   // define the spark maxes
-  private final CANSparkMax m_leftMaster = new CANSparkMax(Definitions.m_leftMaster, CANSparkMax.MotorType.kBrushless);
-  private final CANSparkMax m_leftSlave = new CANSparkMax(Definitions.m_leftSlave, CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax m_leftMaster  = new CANSparkMax(Definitions.m_leftMaster, CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax m_leftSlave   = new CANSparkMax(Definitions.m_leftSlave, CANSparkMax.MotorType.kBrushless);
   private final CANSparkMax m_rightMaster = new CANSparkMax(Definitions.m_rightMaster, CANSparkMax.MotorType.kBrushless);
-  private final CANSparkMax m_rightSlave = new CANSparkMax(Definitions.m_rightSlave, CANSparkMax.MotorType.kBrushless);
-
-  // private final CANSparkMax spinny = new CANSparkMax(Definitions.spinny, CANSparkMax.MotorType.kBrushed);
-  // // setup talon srx
-  private final CANSparkMax uppy = new CANSparkMax(Definitions.uppy, CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax m_rightSlave  = new CANSparkMax(Definitions.m_rightSlave, CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax uppy          = new CANSparkMax(Definitions.uppy, CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax elbowMotor    = new CANSparkMax(Definitions.armPivot, CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax wristMotor    = new CANSparkMax(Definitions.clawPivot, CANSparkMax.MotorType.kBrushless);
 
   // define the joysticks
   private final Joystick c_joystick = new Joystick(Definitions.c_joystick);
-  //private final Joystick armJoystick = new Joystick(Definitions.armJoystick);
   private final PS4Controller c_ps4 = new PS4Controller(Definitions.c_ps4);
 
   // define the differential drive
@@ -120,68 +60,28 @@ public class Robot extends TimedRobot {
   public Servo servo1;
   public Servo servo2;
 
-  public boolean prevTurnMode = false;
-  public boolean turnMode = false;
-  public boolean invTurning = false;
-  public boolean turning = false;
-  public boolean turnLeft = false;
-  public boolean turnRight = false;
+  public boolean prevTurnMode   = false;
+  public boolean turnMode       = false;
+  public boolean invTurning     = false;
+  public boolean turning        = false;
+  public boolean turnLeft       = false;
+  public boolean turnRight      = false;
   public boolean throttleLever0 = false;
 
-  public double lastAccelY = 0;
-  public double lastDriveY = 0;
-
-  public boolean impact = false;
-
-  public int cycle = 0;
-  public int lastCycle = 0;
-
-  public int watch = 0;
-
-  public double leftPower = m_leftMaster.getOutputCurrent();
+  public double leftPower  = m_leftMaster.getOutputCurrent();
   public double rightPower = m_rightMaster.getOutputCurrent();
 
   public double driveX = -c_joystick.getX();
   public double driveY = -c_joystick.getY();
   public double driveZ = -c_joystick.getZ();
 
-  public double armX;
-  public double armY;
-  public double arm3;
-
-  public double accelX = 0;
-  public double accelY = 0;
-  public double accelZ = 0;
-
-  public int loop = 0;
-  public boolean loopToggle = false;
-  public boolean move = false;
-
-  public boolean prev10 = false;
-
-  public boolean clawClosed = false;
-  public double clawAngleMax = 180;
-  public double clawAngleMin = 90;
-  public double clawAngle = 0;
-  public double clawCloseMult = 10;
-
-
   //private DigitalInput baseLimit;
   private DigitalInput elevLimitB;
   private DigitalInput elevLimitT;
   private DigitalInput elbowLimit;
 
-  private CANSparkMax elbowMotor = new CANSparkMax(Definitions.armPivot, CANSparkMax.MotorType.kBrushless);
-  private CANSparkMax wristMotor = new CANSparkMax(Definitions.clawPivot, CANSparkMax.MotorType.kBrushless);
-
-  double elbowPos;
-  double wristPos;
-  double uppyPos;
-
   private Claw_Servo cl_servoR = new Claw_Servo(Definitions.clawServo1);
   private Claw_Servo cl_servoL = new Claw_Servo(Definitions.clawServo2);
-
-  private int i = 0;
 
   private HttpCamera limelightFeed;
 
@@ -199,30 +99,9 @@ public class Robot extends TimedRobot {
 
     cl_servoR.setOpen(Definitions.servo1Open);
     cl_servoL.setOpen(Definitions.servo2Open);
-    // Create an instance of the I2C class for the gyroscope
-    // I2C gyro = new I2C(I2C.Port.kOnboard, SensorConstants.ITG3205_ADDRESS);
-
-    // // Define the register address to read from
-    // byte register = SensorConstants.ITG3205_DATA;
-
-    // // Create a buffer to hold the data read from the gyroscope
-    // byte[] buffer = new byte[SensorConstants.ITG3205_G_TO_READ];
-
-    // // Write to the register to initiate a read of the gyroscope data
-    // gyro.write(register, SensorConstants.ITG3205_G_TO_READ);
-
-    // // Read the data from the gyroscope
-    // gyro.read(register, SensorConstants.ITG3205_G_TO_READ, buffer);
-
-
-    // // Parse the data for each axis (x, y, z) from the buffer
-    // int x = (buffer[0] << 8) | buffer[1];
-    // int y = (buffer[2] << 8) | buffer[3];
-    // int z = (buffer[4] << 8) | buffer[5];
 
     limelightFeed = new HttpCamera("limelight", "http://limelight.local:5800/stream.mjpg");
-    // driverShuffleboardTab.add("LL", limelightFeed).withPosition(0, 0).withSize(15, 8).withProperties(Map.of("Show Crosshair", true, "Show Controls", false));
-    
+
     // define the limiters
     elbowLimit = new DigitalInput(Definitions.elbowLimit);
     elevLimitB = new DigitalInput(Definitions.elevLimitB);
@@ -289,33 +168,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putBoolean("Turning", turning);
     SmartDashboard.putBoolean("Turn Left", turnLeft);
     SmartDashboard.putBoolean("Turn Right", turnRight);
-    SmartDashboard.putBoolean("Throttle lever 0", throttleLever0);
     SmartDashboard.putData("Drivetrain", d_drive);
-    // SmartDashboard.putBoolean("Top limiter", elevLimitT.get());
-    // SmartDashboard.putBoolean("Bottom Limiter", elevLimitB.get());
-    // SmartDashboard.putBoolean("Base limiter", baseLimit.get());
-
-    // pulls data from limelight
-    // double targetValid = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
-    // double targetArea = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
-
-    // displays an indicator when the speed multiplier is too low
-    // if (axis3 <= 0.12)
-    // {
-    //   throttleLever0 = true;
-    // } else
-    // {
-    //   throttleLever0 = false;
-    // }
-
-    // toggles the turning inversion
-    //f (turnMode == true && prevTurnMode == false) { invTurning = !invTurning; }
-
-    // stores the current status of the turn mode button
-    //prevTurnMode = c_joystick.getRawButton(12);
-
-    // if (axis3 <= 0.12)    { throttleLever0 = true; }
-    // else    { throttleLever0 = false; }
   }
 
 
@@ -345,7 +198,6 @@ public class Robot extends TimedRobot {
     timer.reset();
     timer.start();
 
-    i = 0;
     Stage = 1;
   }
 
